6fa89cf7c3880cf0fe05fd24abc5d78d945c19e1
leobewater <mmbee888@gmail.com>
2024-02-13 14:52:08 -0500

    show organization icon

--- app/(dashboard)/_components/sidebar/list.tsx
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Item } from "@/app/(dashboard)/_components/sidebar/item";
 import { useOrganizationList } from "@clerk/nextjs";
 
 export const List = () => {
@@ -14,7 +15,12 @@ export const List = () => {
   return (
     <ul className="space-y-4">
       {userMemberships.data?.map((mem) => (
-        <p key={mem.organization.id}>{mem.organization.name}</p>
+        <Item
+          key={mem.organization.id}
+          id={mem.organization.id}
+          name={mem.organization.name}
+          imageUrl={mem.organization.imageUrl}
+        />
       ))}
     </ul>
   );
