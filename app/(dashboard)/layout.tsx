34ff2f585b98b4afd93b8011fc529d7d0659e81f
leobewater <mmbee888@gmail.com>
2024-02-13 12:27:57 -0500

    add orgsidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { OrgSidebar } from "@/app/(dashboard)/_components/org-sidebar";
 import { Sidebar } from "@/app/(dashboard)/_components/sidebar";
 
 type DashboardLayoutProps = {
@@ -5,10 +6,19 @@ type DashboardLayoutProps = {
 };
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-  return <main className="h-full">
-    <Sidebar />
-    <div className="pl-[60px] h-full">{children}</div>
-  </main>;
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            {/* Add Navbar */}
+            {children}</div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default DashboardLayout;
