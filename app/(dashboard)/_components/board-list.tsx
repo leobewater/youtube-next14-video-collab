278e0a668c8c83f696007bd32af62710a387dc9b
leobewater <mmbee888@gmail.com>
2024-02-14 09:30:00 -0500

    add empty search component

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { EmptySearch } from "@/app/(dashboard)/_components/empty-search";
+
 interface BoardListProps {
   orgId: string;
   query: {
@@ -9,5 +11,22 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = [];
+
+  // search returns no results
+  if (!data?.length && query.search) {
+    return <EmptySearch />
+  }
+  
+  // no favorites
+  if (!data?.length && query.favorites) {
+    return <div>No favorites</div>;
+  }
+  
+  // no boards
+  if (!data?.length) {
+    return <div>No boards at all</div>;
+  }
+
   return <div>{JSON.stringify(query)}</div>;
 };
