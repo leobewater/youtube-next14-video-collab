295c33e81e0f7515e7256a8525e46147670ce619
leobewater <mmbee888@gmail.com>
2024-02-14 08:35:32 -0500

    only show invite member button when there is organization

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,39 +2,45 @@
 
 import { InviteButton } from "@/app/(dashboard)/_components/invite-button";
 import { SearchInput } from "@/app/(dashboard)/_components/search-input";
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
 
 export const Navbar = () => {
+  const organization = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
-        <SearchInput / >
+        <SearchInput />
       </div>
       <div className="block lg:hidden flex-1">
-      <OrganizationSwitcher
-        hidePersonal
-        appearance={{
-          elements: {
-            rootBox: {
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%",
-              maxWidth: "376px",
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
             },
-            organizationSwitcherTrigger: {
-              padding: "6px",
-              width: "100%",
-              borderRadius: "8px",
-              border: "1px solid #E5E7EB",
-              justifyContent: "space-between",
-              backgroundColor: "white",
-            },
-          },
-        }}
-      />
+          }}
+        />
       </div>
-      <InviteButton />
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );
