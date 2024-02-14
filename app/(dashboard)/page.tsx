f13441060614cda7038f883d877a8a2e7aa953b4
leobewater <mmbee888@gmail.com>
2024-02-14 08:48:40 -0500

    add board list component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import { BoardList } from "@/app/(dashboard)/_components/board-list";
 import { EmptyOrg } from "@/app/(dashboard)/_components/empty-org";
 import { useOrganization } from "@clerk/nextjs";
 
-export default function DashboardPage() {
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
 
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      {!organization ? <EmptyOrg /> : <p>Board List!</p>}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
-}
+};
+
+export default DashboardPage;
