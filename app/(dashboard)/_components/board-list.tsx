3feaa5b2d7e2f29da136bdcb594fed0c986b5596
leobewater <mmbee888@gmail.com>
2024-02-14 09:33:38 -0500

    add empty favorites and boards components

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { EmptyBoards } from "@/app/(dashboard)/_components/empty-boards";
+import { EmptyFavorites } from "@/app/(dashboard)/_components/empty-favorites";
 import { EmptySearch } from "@/app/(dashboard)/_components/empty-search";
 
 interface BoardListProps {
@@ -20,12 +22,12 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   
   // no favorites
   if (!data?.length && query.favorites) {
-    return <div>No favorites</div>;
+    return <EmptyFavorites />
   }
   
   // no boards
   if (!data?.length) {
-    return <div>No boards at all</div>;
+    return <EmptyBoards />
   }
 
   return <div>{JSON.stringify(query)}</div>;
