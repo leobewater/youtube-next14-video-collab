c56841df150f1417d09aaec64326a1a92e897317
leobewater <mmbee888@gmail.com>
2024-02-14 11:51:56 -0500

    add card hover over menu

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -3,10 +3,12 @@
 import { formatDistanceToNow } from "date-fns";
 import Image from "next/image";
 import Link from "next/link";
-import { Overlay } from "@/app/(dashboard)/_components/board-card/overlay";
 import { useAuth } from "@clerk/nextjs";
+import { Overlay } from "@/app/(dashboard)/_components/board-card/overlay";
 import { Footer } from "@/app/(dashboard)/_components/board-card/footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -40,6 +42,11 @@ export const BoardCard = ({
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
