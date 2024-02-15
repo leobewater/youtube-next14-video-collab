2ccc2acda92eff7c4e6c96fd4a4ca90926582951
leobewater <mmbee888@gmail.com>
2024-02-15 15:08:01 -0500

    add more types and layers

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -18,7 +18,7 @@ interface ToolbarProps {
   redo: () => void;
   canUndo: boolean;
   canRedo: boolean;
-};
+}
 
 export const Toolbar = ({
   canvasState,
@@ -40,8 +40,8 @@ export const Toolbar = ({
         <ToolButton
           label="Text"
           icon={Type}
-          onClick={() => {}}
-          isActive={false}
+          onClick={() => setCanvasState({ mode: CanvasMode.Inserting })}
+          isActive={canvasState.mode === CanvasMode.Inserting}
         />
         <ToolButton
           label="Sticky note"
--- types/canvas.ts
+++ types/canvas.ts
@@ -1,3 +1,73 @@
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+};
+
+export enum LayerType {
+  Rectangle,
+  Ellipse,
+  Path,
+  Text,
+  Note,
+}
+
+export type RectangleLayer = {
+  type: LayerType.Rectangle;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type EllipseLayer = {
+  type: LayerType.Ellipse;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type PathLayer = {
+  type: LayerType.Path;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  points: number[][];
+  value?: string;
+};
+
+export type TextLayer = {
+  type: LayerType.Text;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type NoteLayer = {
+  type: LayerType.Note;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
 export type CanvasState =
   | {
       mode: CanvasMode.None;
