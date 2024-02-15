9aa568cc7857da1269c37c5c35d9574c4cbf332d
leobewater <mmbee888@gmail.com>
2024-02-15 10:50:32 -0500

    use connectionId to get random border color for user avatar

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -3,6 +3,7 @@
 import { useOthers, useSelf } from "@/liveblocks.config";
 
 import { UserAvatar } from "@/app/board/[boardId]/_components/user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
 
 // show max other users, excluding yourself
 const MAX_SHOWN_USERS = 1;
@@ -17,6 +18,7 @@ export const Participants = () => {
       <div className="flex gap-x-2">
         {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => (
           <UserAvatar
+            borderColor={connectionIdToColor(connectionId)}
             key={connectionId}
             src={info?.picture}
             name={info?.name}
@@ -26,6 +28,7 @@ export const Participants = () => {
 
         {currentUser && (
           <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
             src={currentUser.info?.picture}
             name={`${currentUser.info?.name} (You)`}
             fallback={currentUser.info?.name?.[0]}
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,7 @@ const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+}
