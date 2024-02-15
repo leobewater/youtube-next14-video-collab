817f00d46bebea17734568e1f04c29883e5b4c72
leobewater <mmbee888@gmail.com>
2024-02-15 10:10:19 -0500

    redo skeleton without using client

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
@@ -8,7 +6,7 @@ export const Info = () => {
   );
 };
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px] bg-muted-400" />
   );
--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -1,17 +1,15 @@
-'use client';
-
-import { Info } from "@/app/board/[boardId]/_components/info";
-import { Participants } from "@/app/board/[boardId]/_components/participants";
-import { Toolbar } from "@/app/board/[boardId]/_components/toolbar";
+import { InfoSkeleton } from "@/app/board/[boardId]/_components/info";
+import { ParticipantsSkeleton } from "@/app/board/[boardId]/_components/participants";
+import { ToolbarSkeleton } from "@/app/board/[boardId]/_components/toolbar";
 import { Loader } from "lucide-react";
 
 export const Loading = () => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
-      <Info.Skeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skeleton />
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 };
