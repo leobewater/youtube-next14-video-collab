875d9787651d529daca2c397ea0b44e8eaa44525
leobewater <mmbee888@gmail.com>
2024-02-15 09:42:11 -0500

    add convex get endpoint

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "@/convex/_generated/server";
+import { mutation, query } from "@/convex/_generated/server";
 
 const images = [
   "/placeholders/1.svg",
@@ -158,9 +158,8 @@ export const unfavorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex(
-        "by_user_board",
-        (q) => q.eq("userId", userId).eq("boardId", board._id)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
@@ -169,6 +168,15 @@ export const unfavorite = mutation({
     }
 
     await ctx.db.delete(existingFavorite._id);
+
+    return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const board = ctx.db.get(args.id);
 
     return board;
   },
