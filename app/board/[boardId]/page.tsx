66a0b7a2580a030fee822e5d76c72b060c314aa7
leobewater <mmbee888@gmail.com>
2024-02-15 06:40:21 -0500

    make room's fallback as prop

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,12 +1,18 @@
 import { Canvas } from "@/app/board/[boardId]/_components/canvas";
+import { Room } from "@/components/room";
 
 interface BoardIdPageProps {
   params: {
     boardId: string;
   };
 }
+
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return <Canvas boardId={params.boardId} />;
+  return (
+    <Room roomId={params.boardId} fallback={<div>Loading...</div>}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
 };
 
 export default BoardIdPage;
