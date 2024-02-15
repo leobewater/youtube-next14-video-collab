97acc790b34033526abe06da4dc72079737c7f23
leobewater <mmbee888@gmail.com>
2024-02-15 06:28:29 -0500

    add toolbar and participants

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Info } from "@/app/board/[boardId]/_components/info";
+import { Participants } from "@/app/board/[boardId]/_components/participants";
+import { Toolbar } from "@/app/board/[boardId]/_components/toolbar";
 
 export const Canvas = () => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info />
+      <Participants />
+      <Toolbar />
     </main>
   );
 };
