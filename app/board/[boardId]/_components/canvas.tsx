1589ee6321a2e56f13f0a0117fa1ed5cc35ff8b2
leobewater <mmbee888@gmail.com>
2024-02-15 06:23:38 -0500

    add info

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,11 @@
-'use client';
+"use client";
+
+import { Info } from "@/app/board/[boardId]/_components/info";
 
 export const Canvas = () => {
   return (
-    <div>Canvas!</div>
-
-  )
-}
+    <main className="h-full w-full relative bg-neutral-100 touch-none">
+      <Info />
+    </main>
+  );
+};
