7673ff85ab9e3dc8ae94697e8d2ea86b35ba4f89
leobewater <mmbee888@gmail.com>
2024-02-15 10:02:30 -0500

    get name and picture from liveblocks

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,12 +3,15 @@
 import { Info } from "@/app/board/[boardId]/_components/info";
 import { Participants } from "@/app/board/[boardId]/_components/participants";
 import { Toolbar } from "@/app/board/[boardId]/_components/toolbar";
+import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const { name, picture } = useSelf((me) => me.info);
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info />
