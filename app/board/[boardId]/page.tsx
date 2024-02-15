82b5761a33a09abdb0b872af6f3ef40078e87ad5
leobewater <mmbee888@gmail.com>
2024-02-15 06:57:19 -0500

    add skeleton functions for loading

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -9,6 +9,7 @@ interface BoardIdPageProps {
 }
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  // return <Loading />
   return (
     <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
